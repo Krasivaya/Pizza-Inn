efcea9482ce56eefea4bdcff25e85865b548b0e1
Krasivaya <semwagacarine@gmail.com>
2019-08-08 21:54:08 +0200

    Add jquery

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
-var sizes = ["small", "medium" , "large"]
 $(document).ready(function(){
   var Amount = 0;
-  $("#submit").click(function(){
-      var checkVal = $("input[name=Yes]:checked").val();
-      if (checkVal === "yes"){
-          Amount = 1;
-          var getLocation = prompt("Let us know your location:");
-          alert(" Your pizza will be delivered at " + getLocation + " with additional amount of 1$ for delivery");
-      }
-  })
+  var AmountT = 0;
   $("#submit").click(function(event){
-  var Size = parseFloat(document.getElementById('size').value);
-  var Crust = parseFloat(document.getElementById('crust').value);
-  var Topping = parseFloat(document.getElementById('toppings').value);
-  var Quantity = parseFloat(document.getElementById('num').value);
-  var total = (Size + Crust + Topping + Quantity + Amount);
+    var cLct = $("input[name=Yes]:checked").val();
+    if (cLct === "Yes"){
+        Amount = 1000;
+        var getLct = prompt("Your location:");
+        alert("Your order will be delivered to " + getLct + "\nDelivery amount is 1000 RWF");
+    }
+
+  var Sizev = parseFloat(document.getElementById('size').value);
+  var Size = document.getElementById('size').value;
+  var Crustv = parseFloat(document.getElementById('crust').value);
+  var Crust = document.getElementById('crust').value;
+  var Toppingsv = parseFloat(document.getElementById('toppings').value);
+  var Toppings = document.getElementById('toppings').value;
+  var tl = (Sizev + Crustv + Toppingsv + Amount);
+  var total = (tl * Nbrv);
 
   
-  document.getElementById("getsize").innerHTML =  Size;
-  document.getElementById("getcrust").innerHTML =   Crust;
-  document.getElementById("gettopping").innerHTML =  Topping;
-  document.getElementById("sum").innerHTML = total;
-  $(".the-order").show();
-  event.preventDefault();
+  document.getElementById("a1").innerHTML =  Size;
+  document.getElementById("a2").innerHTML =   Crust;
+  var Nbrv = parseFloat(document.getElementById("nbr").value);
+  var Nbr = document.getElementById('nbr').value;
+  document.getElementById("a7").innerText =  Nbr;
+  document.getElementById("a3").innerHTML =  Toppings;
+  document.getElementById("dl").innerHTML =  tl;
   })
 })
+
 
